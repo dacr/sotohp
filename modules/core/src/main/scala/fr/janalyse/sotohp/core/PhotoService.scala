b77f3e74b232cdeed521ae9d492be4467dbfc705
Crosson David <crosson.david@gmail.com>
2023-08-19 23:33:14 +0200

    Prototype PhotoService interface

--- modules/core/src/main/scala/fr/janalyse/sotohp/core/PhotoService.scala
+++ modules/core/src/main/scala/fr/janalyse/sotohp/core/PhotoService.scala
@@ -4,15 +4,25 @@ import zio.*
 import zio.stream.*
 import fr.janalyse.sotohp.model.*
 
+import java.time.OffsetDateTime
+
 case class PhotoServiceUserIssue(message: String)
 case class PhotoServiceSystemIssue(message: String)
 type PhotoServiceIssue = PhotoServiceUserIssue | PhotoServiceSystemIssue
 
+case class TimeRange(
+  start: Option[OffsetDateTime],
+  end: Option[OffsetDateTime]
+)
+
 case class PhotoSimpleQuery(
-  keywords: Option[List[String]]
+  ownerId: Option[PhotoOwnerId],
+  keywords: Option[List[String]],
+  timeRange: Option[TimeRange]
 )
 
 case class CategorySimpleQuery(
+  ownerId: Option[PhotoOwnerId],
   keywords: Option[List[String]]
 )
 
