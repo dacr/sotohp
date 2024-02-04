4df8126904f77524ed6c9f127174308742434831
Crosson David <crosson.david@gmail.com>
2024-02-04 15:34:32 +0100

    Enhance statistics

--- user-interfaces/cli/src/main/scala/fr/janalyse/sotohp/cli/Statistics.scala
+++ user-interfaces/cli/src/main/scala/fr/janalyse/sotohp/cli/Statistics.scala
@@ -19,7 +19,8 @@ case class Statistics(
   duplicated: Map[String, Int] = Map.empty,
   missingCount: Int = 0,
   modifiedCount: Int = 0,
-  missingShootingDate: Int = 0
+  missingShootingDate: Int = 0,
+  invalidShootingDateCount: Int = 0
 )
 
 object Statistics extends ZIOAppDefault with CommonsCLI {
@@ -35,6 +36,8 @@ object Statistics extends ZIOAppDefault with CommonsCLI {
         Scope.default
       )
 
+  val shootingDateMinimumValidYear = 1826 // https://en.wikipedia.org/wiki/History_of_photography
+
   def updateStats(stats: Statistics, zphoto: LazyPhoto) = {
     for {
       source           <- zphoto.source.some
@@ -48,19 +51,20 @@ object Statistics extends ZIOAppDefault with CommonsCLI {
       originalModified <- ZIO
                             .attempt(source.fileLastModified.toInstant.toEpochMilli != source.original.path.toFile.lastModified())
                             .when(originalFound)
-                            .someOrElse(true)
+                            .someOrElse(false)
     } yield {
-      val updatedCount                  = stats.count + 1
-      val updatedGeolocalizedCount      = stats.geolocalizedCount + (if (place.isDefined) 1 else 0)
-      val updatedNormalizedFailureCount = stats.normalizedFailureCount + (if (hasNormalized) 0 else 1)
-      val updatedFacesCount             = stats.facesCount + faces.map(_.count).getOrElse(0)
-      val updatedMissingCount           = stats.missingCount + (if (originalFound) 0 else 1)
-      val updatedModifiedCount          = stats.modifiedCount + (if (originalModified) 1 else 0)
-      val updatedDuplicated             = stats.duplicated + (stats.duplicated.get(filehash) match {
+      val updatedCount                    = stats.count + 1
+      val updatedGeolocalizedCount        = stats.geolocalizedCount + (if (place.isDefined) 1 else 0)
+      val updatedNormalizedFailureCount   = stats.normalizedFailureCount + (if (hasNormalized) 0 else 1)
+      val updatedFacesCount               = stats.facesCount + faces.map(_.count).getOrElse(0)
+      val updatedMissingCount             = stats.missingCount + (if (originalFound) 0 else 1)
+      val updatedModifiedCount            = stats.modifiedCount + (if (originalModified) 1 else 0)
+      val updatedDuplicated               = stats.duplicated + (stats.duplicated.get(filehash) match {
         case None        => filehash -> 1
         case Some(count) => filehash -> (count + 1)
       })
-      val updatedMissingShootingDate    = stats.missingShootingDate + (if (shootingDate.isEmpty) 1 else 0)
+      val updatedMissingShootingDateCount = stats.missingShootingDate + (if (shootingDate.isEmpty) 1 else 0)
+      val updatedInvalidShootingDateCount = stats.invalidShootingDateCount + (if (shootingDate.exists(_.getYear < shootingDateMinimumValidYear)) 1 else 0)
       stats.copy(
         count = updatedCount,
         geolocalizedCount = updatedGeolocalizedCount,
@@ -69,23 +73,26 @@ object Statistics extends ZIOAppDefault with CommonsCLI {
         facesCount = updatedFacesCount,
         missingCount = updatedMissingCount,
         modifiedCount = updatedModifiedCount,
-        missingShootingDate = updatedMissingShootingDate
+        missingShootingDate = updatedMissingShootingDateCount,
+        invalidShootingDateCount = updatedInvalidShootingDateCount
       )
     }
   }
 
   def reportStats(stats: Statistics) = {
+    import stats.*
     val duplicatedCount = stats.duplicated.filter((_, count) => count > 1).size
     for {
       _ <- Console.printLine(s"${UNDERLINED}${BLUE}Photo statistics :$RESET")
-      _ <- Console.printLine(s"${GREEN}- ${stats.count} photos$RESET")
-      _ <- Console.printLine(s"${GREEN}- ${stats.facesCount} people faces$RESET")
-      _ <- Console.printLine(s"${GREEN}- ${stats.geolocalizedCount} geolocalized photos$RESET")
-      _ <- Console.printLine(s"${RED}- ${stats.normalizedFailureCount} normalization failures$RESET")
-      _ <- Console.printLine(s"${RED}- ${stats.missingCount} missing originals !!$RESET")
-      _ <- Console.printLine(s"${YELLOW}- ${stats.modifiedCount} modified originals$RESET")
-      _ <- Console.printLine(s"${YELLOW}- $duplicatedCount duplicated photos$RESET")
-      _ <- Console.printLine(s"${YELLOW}- ${stats.missingShootingDate} photos without shooting date$RESET")
+      _ <- Console.printLine(s"${GREEN}- $count photos$RESET")
+      _ <- Console.printLine(s"${GREEN}- $facesCount people faces$RESET")
+      _ <- Console.printLine(s"${GREEN}- $geolocalizedCount geolocalized photos $YELLOW(${count-geolocalizedCount} without GPS infos)$RESET")
+      _ <- Console.printLine(s"${YELLOW}- $duplicatedCount duplicated photos$RESET").when(duplicatedCount > 0)
+      _ <- Console.printLine(s"${YELLOW}- $missingShootingDate photos without shooting date$RESET").when(missingShootingDate > 0)
+      _ <- Console.printLine(s"${YELLOW}- $modifiedCount modified originals$RESET").when(modifiedCount > 0)
+      _ <- Console.printLine(s"${RED}- $missingCount missing originals !!$RESET").when(missingCount > 0)
+      _ <- Console.printLine(s"${RED}- $invalidShootingDateCount invalid shooting date year (< $shootingDateMinimumValidYear)$RESET").when(invalidShootingDateCount > 0)
+      _ <- Console.printLine(s"${RED}- $normalizedFailureCount normalization failures (original file issue)$RESET").when(normalizedFailureCount > 0)
     } yield stats
   }
 
