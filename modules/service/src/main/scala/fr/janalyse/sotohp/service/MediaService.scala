6def99ae9b5d56686bb462bf2c2e5f0f749dfd0f
crosson david lanfeust <crosson.david@gmail.com>
2025-07-24 21:27:30 +0200

    Add horizontal separators in `MediaService` for improved section clarity and organization.

--- modules/service/src/main/scala/fr/janalyse/sotohp/service/MediaService.scala
+++ modules/service/src/main/scala/fr/janalyse/sotohp/service/MediaService.scala
@@ -90,6 +90,7 @@ trait MediaService {
   def synchronize(): IO[ServiceIssue, Unit]
 }
 
+
 object MediaService {
 
   val live: ZLayer[LMDB, LMDBIssues, MediaService] = ZLayer.fromZIO(
@@ -98,6 +99,8 @@ object MediaService {
       mediaServiceLive <- MediaServiceLive.setup(lmdb)
     } yield mediaServiceLive
   )
+
+  // -------------------------------------------------------------------------------------------------------------------
 
   def mediaFind(nearKey: MediaAccessKey, ownerId: Option[OwnerId]): ZIO[MediaService, ServiceIssue, Option[Media]] = ZIO.serviceWithZIO(_.mediaFind(nearKey, ownerId))
 
@@ -125,11 +128,15 @@ object MediaService {
   ): ZIO[MediaService, ServiceIssue, Option[Media]] =
     ZIO.serviceWithZIO(_.mediaUpdate(key, eventId, description, starred, keywords, orientation, shootDateTime, location))
 
+  // -------------------------------------------------------------------------------------------------------------------
+
   def mediaNormalizedRead(key: MediaAccessKey): ZIO[MediaService, ServiceIssue, Stream[ServiceStreamIssue, Byte]] = ZIO.serviceWithZIO(_.mediaNormalizedRead(key))
 
   def mediaOriginalRead(key: MediaAccessKey): ZIO[MediaService, ServiceIssue, Stream[ServiceStreamIssue, Byte]] = ZIO.serviceWithZIO(_.mediaOriginalRead(key))
 
   def mediaMiniatureRead(key: MediaAccessKey): ZIO[MediaService, ServiceIssue, Stream[ServiceStreamIssue, Byte]] = ZIO.serviceWithZIO(_.mediaMiniatureRead(key))
+
+  // -------------------------------------------------------------------------------------------------------------------
 
   def eventList(): ZIO[MediaService, ServiceIssue, Stream[ServiceStreamIssue, Event]] = ZIO.serviceWithZIO(_.eventList())
 
@@ -142,6 +149,8 @@ object MediaService {
 
   def eventUpdate(eventId: EventId, name: EventName, description: Option[EventDescription], keywords: Set[Keyword]): ZIO[MediaService, ServiceIssue, Option[Event]] = ZIO.serviceWithZIO(_.eventUpdate(eventId, name, description, keywords))
 
+  // -------------------------------------------------------------------------------------------------------------------
+
   def ownerList(): ZIO[MediaService, ServiceIssue, List[Owner]] = ZIO.serviceWithZIO(_.ownerList())
 
   def ownerGet(ownerId: OwnerId): ZIO[MediaService, ServiceIssue, Option[Owner]] = ZIO.serviceWithZIO(_.ownerGet(ownerId))
@@ -151,6 +160,8 @@ object MediaService {
   def ownerCreate(providedOwnerId: Option[OwnerId], firstName: FirstName, lastName: LastName, birthDate: Option[BirthDate]): ZIO[MediaService, ServiceIssue, Owner] = ZIO.serviceWithZIO(_.ownerCreate(providedOwnerId, firstName, lastName, birthDate))
 
   def ownerUpdate(ownerId: OwnerId, firstName: FirstName, lastName: LastName, birthDate: Option[BirthDate]): ZIO[MediaService, ServiceIssue, Option[Owner]] = ZIO.serviceWithZIO(_.ownerUpdate(ownerId, firstName, lastName, birthDate))
+
+  // -------------------------------------------------------------------------------------------------------------------
 
   def storeList(): ZIO[MediaService, ServiceIssue, List[Store]] = ZIO.serviceWithZIO(_.storeList())
 
@@ -162,6 +173,8 @@ object MediaService {
     ZIO.serviceWithZIO(_.storeCreate(providedStoreId, ownerId, baseDirectory, includeMask, ignoreMask))
 
   def storeUpdate(storeId: StoreId, includeMask: Option[IncludeMask], ignoreMask: Option[IgnoreMask]): ZIO[MediaService, ServiceIssue, Option[Store]] = ZIO.serviceWithZIO(_.storeUpdate(storeId, includeMask, ignoreMask))
+
+  // -------------------------------------------------------------------------------------------------------------------
 
   def synchronize(): ZIO[MediaService, ServiceIssue, Unit] = ZIO.serviceWithZIO(_.synchronize())
 }
