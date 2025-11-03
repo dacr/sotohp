59675b193e8f2218b8d8faea4600bfdbd963c489
crosson david lanfeust <crosson.david@gmail.com>
2025-11-03 18:04:02 +0100

    Switch to `zio-logging-slf4j2-bridge` and update logging layer implementation

--- user-interfaces/cli/src/main/scala/fr/janalyse/sotohp/cli/CommonsCLI.scala
+++ user-interfaces/cli/src/main/scala/fr/janalyse/sotohp/cli/CommonsCLI.scala
@@ -4,14 +4,16 @@ import zio.*
 import zio.Runtime.removeDefaultLoggers
 import zio.config.typesafe.TypesafeConfigProvider
 import zio.logging.LogFormat
-import zio.logging.backend.SLF4J
+//import zio.logging.backend.SLF4J
 
 abstract class CommonsCLI extends ZIOAppDefault {
   val configProvider = TypesafeConfigProvider.fromTypesafeConfig(com.typesafe.config.ConfigFactory.load())
   val configProviderLayer = Runtime.setConfigProvider(configProvider)
 
+  //val loggingLayer = removeDefaultLoggers >>> SLF4J.slf4j(format = LogFormat.colored)
+  val loggingLayer = zio.logging.slf4j.bridge.Slf4jBridge.initialize
+
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Any] = {
-    val loggingLayer = removeDefaultLoggers >>> SLF4J.slf4j(format = LogFormat.colored)
     loggingLayer ++ configProviderLayer
   }
 }
